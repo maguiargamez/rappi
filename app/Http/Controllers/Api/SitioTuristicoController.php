012e71a1ab36a8850abfa6581852dc4e281cd375
[manuel] <[manuelaguiargamez@gmail.com]>
2021-11-22 22:43:06 -0600

    M4

--- app/Http/Controllers/Api/SitioTuristicoController.php
+++ app/Http/Controllers/Api/SitioTuristicoController.php
@@ -8,15 +8,36 @@ use App\Http\Resources\SitioTuristicoResource;
 use App\Models\SitioTuristico;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(title="API Usuarios", version="1.0")
+ *
+ * @OA\Server(url="http://raapi.test")
+ */
+
 class SitioTuristicoController extends Controller
 {
-    public function show($id)
-    {
-        $sitio= SitioTuristico::getSingle($id);
-        $sitio->region= $sitio->region->nombre;
-        return SitioTuristicoResource::make($sitio);
-    }
 
+    /**
+     * @OA\Get(
+     *      path="/api/v1/sitios-turisticos/",
+     *      operationId="obtenerSitiosTuristico",
+     *      tags={"Sitios turisticos"},
+     *      summary="Obtiene todos los sistios turisticos",
+     *      description="Regresa los sitios turisticos",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Éxito"
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
     public function index()
     {
         $sitios= SitioTuristico::getAll();
@@ -24,5 +45,38 @@ class SitioTuristicoController extends Controller
             $sitio->region= $sitio->region->nombre;
         }
         return SitioTuristicoCollection::make($sitios);
+    }
+
+    /**
+     * @OA\Get(
+     *     path="/api/v1/sitios-turisticos/{id}",
+     *     operationId="obtenerSitioTuristicoPorId",
+     *     tags={"Sitios turisticos"},
+     *     summary="Mostrar un solo sitio turistico",
+     *     description="Regresa el sitio turistico especifico",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Id del sitio turistico",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Éxito"
+     *     ),
+     *     @OA\Response(
+     *         response="default",
+     *         description="Ha ocurrido un error."
+     *     )
+     * )
+     */
+    public function show($id)
+    {
+        $sitio= SitioTuristico::getSingle($id);
+        $sitio->region= $sitio->region->nombre;
+        return SitioTuristicoResource::make($sitio);
     }
 }
--- app/Http/Controllers/Api/SitioTuristicoVisitaController.php
+++ app/Http/Controllers/Api/SitioTuristicoVisitaController.php
@@ -9,6 +9,73 @@ use Illuminate\Http\Request;
 
 class SitioTuristicoVisitaController extends Controller
 {
+    /**
+     * @OA\Post(
+     *      path="/api/v1/sitios-turisticos-visitas/",
+     *      operationId="agregarSitioTuristicoVisita",
+     *      tags={"Sitios turisticos"},
+     *      summary="Agrega una visita al sistio turistico",
+     *      description="Regresa el sitio turistico",
+     *      @OA\Parameter(
+     *     name="dato",
+     *     in="path",
+     *     description="Objeto JSON con datos de la visita",
+     *     required=true,
+     *     @OA\Schema(
+     *       @OA\Property(
+     *         property="data",
+     *         type="object",
+     *         @OA\Property(
+     *           property="type",
+     *           type="string",
+     *           default="sitios-turisticos-visitas",
+     *           example="sitios-turisticos-visitas",
+     *         ),
+     *         @OA\Property(
+     *           property="attributes",
+     *           type="object",
+     *           @OA\Property(
+     *             property="sitio_turistico_id",
+     *             type="number",
+     *             default=1,
+     *             example=1
+     *           ),
+     *           @OA\Property(
+     *             property="ip",
+     *             type="string",
+     *             example="192.168.1.1"
+     *           ),
+     *           @OA\Property(
+     *             property="fecha",
+     *             type="datetime",
+     *
+     *             example="2021-11-19 13:39:00"
+     *           ),
+     *         )
+
+     *       )
+     *     )
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="Details of the customer"
+     *   ),
+     *   @OA\Response(
+     *     response=400,
+     *     description="Email required"
+     *   ),
+     *   @OA\Response(
+     *     response=404,
+     *     description="Customer does not exist"
+     *   ),
+     *   @OA\Response(
+     *     response="default",
+     *     description="an ""unexpected"" error"
+     *   )
+     * )
+     */
+
+
     public function create(Request $request)
     {
         $visita = SitioTuristicoVisita::create([
